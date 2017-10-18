2033e205587817dc68d0a9921c829a25136955a9
michaelzhang <heronalps@gmail.com>
2017-10-17 21:23:07 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
 
   get 'users/new'
+  post '/cars', to: 'cars#create'
 
   resources :rental_posts
   resources :cars
   resources :users
-  
+
   root 'users#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

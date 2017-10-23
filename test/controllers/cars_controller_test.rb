3b99563ccc161a711649e0d657e3dfb4d9aa2af9
michaelzhang <heronalps@gmail.com>
2017-10-23 13:13:33 -0700

    Prepare for eb

---
+++ test/controllers/cars_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class CarsControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @user = User.create!(name: "Example User", email:"user@example.com",
+                        password: "foobar", password_confirmation: "foobar")
+    @car = Car.create!(user_id: @user.id, plate_num: "m123", model: "chev", color: "red", year: 1)
+  end
+
+  test "should get index" do
+    get cars_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_car_url
+    assert_response :success
+  end
+
+  test "should create car" do
+    assert_difference('Car.count') do
+      post cars_url, params: { car: { color: @car.color, model: @car.model, plate_num: @car.plate_num, user_id: @car.user_id, year: @car.year } }
+    end
+
+    assert_redirected_to car_url(Car.last)
+  end
+
+  test "should show car" do
+    get car_url(@car)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_car_url(@car)
+    assert_response :success
+  end
+
+  test "should update car" do
+    patch car_url(@car), params: { car: { color: @car.color, model: @car.model, plate_num: @car.plate_num, user_id: @car.user_id, year: @car.year } }
+    assert_redirected_to car_url(@car)
+  end
+
+  test "should destroy car" do
+    assert_difference('Car.count', -1) do
+      delete car_url(@car)
+    end
+
+    assert_redirected_to cars_url
+  end
+end

0d90448e11196da85fc24e1efc4bff7d67121bd7
michaelzhang <heronalps@gmail.com>
2017-11-02 11:26:35 -0700

    Add index in rentalpost table & add rental_post_test

--- app/models/rental_post.rb
+++ app/models/rental_post.rb
@@ -2,4 +2,5 @@ class RentalPost < ApplicationRecord
     has_one :user, through: :owner_id
     has_one :user, through: :renter_id
     has_one :car
+    validates :owner_id, presence: true
 end
--- db/migrate/20171018032037_create_rental_posts.rb
+++ db/migrate/20171018032037_create_rental_posts.rb
@@ -1,9 +1,9 @@
 class CreateRentalPosts < ActiveRecord::Migration[5.0]
   def change
     create_table :rental_posts do |t|
-      t.integer :car_id
-      t.integer :owner_id
-      t.integer :renter_id
+      t.integer :car_id, foreign_key: true
+      t.integer :owner_id, foreign_key: true
+      t.integer :renter_id, foreign_key: true
       t.string :start_location
       t.string :end_location
       t.datetime :start_time
@@ -12,5 +12,6 @@ class CreateRentalPosts < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
+    add_index :rental_posts, [:owner_id, :created_at]
   end
 end

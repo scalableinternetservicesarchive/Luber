bbb0ef0f8cec9322e9ce90f791498f7b14f3b478
Sujaya A Maiyya <sujayamaiya@gmail.com>
2017-11-14 15:02:15 -0800

    Adding missed html file

--- db/migrate/20171102185822_add_columns_to_rental_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToRentalPosts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :rental_posts, :start_latitude, :float
-    add_column :rental_posts, :start_longitude, :float
-  end
-end

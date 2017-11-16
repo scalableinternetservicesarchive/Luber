dc5651c0992930bfafdc42952fe9b21a75ed25a3
masoug <masoug@gmail.com>
2017-11-16 12:06:31 -0800

    added permissions so that users must log in before managing cars. also only owners can edit/delete their cars.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,7 @@
 class CarsController < ApplicationController
   before_action :set_car, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:new, :edit, :create, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def tag_search
     if params[:tag]
@@ -34,11 +36,11 @@ class CarsController < ApplicationController
 
     respond_to do |format|
       if @car.save!
-        format.html { redirect_to @car, notice: 'Car was successfully created.' }
-        format.json { render :show, status: :created, location: @car }
+        format.html {redirect_to @car, notice: 'Car was successfully created.'}
+        format.json {render :show, status: :created, location: @car}
       else
-        format.html { render :new }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @car.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -48,11 +50,11 @@ class CarsController < ApplicationController
   def update
     respond_to do |format|
       if @car.update(car_params)
-        format.html { redirect_to @car, notice: 'Car was successfully updated.' }
-        format.json { render :show, status: :ok, location: @car }
+        format.html {redirect_to @car, notice: 'Car was successfully updated.'}
+        format.json {render :show, status: :ok, location: @car}
       else
-        format.html { render :edit }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @car.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -62,19 +64,38 @@ class CarsController < ApplicationController
   def destroy
     @car.destroy
     respond_to do |format|
-      format.html { redirect_to cars_url, notice: 'Car was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to cars_url, notice: 'Car was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_car
-      @car = Car.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def car_params
-      params.require(:car).permit(:user_id, :plate_number, :model, :color, :year, :all_tags)
+  # before filters for authorization
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in or register before messing with them cars."
+      redirect_to login_url
     end
+  end
+
+  # confirms the correct user
+  def correct_user
+    @car = Car.find(params[:id])
+    @user = User.find(@car.user.id)
+    unless current_user?(@user)
+      flash[:danger] = "You are not the owner of this car! GTFO"
+      redirect_to(@car)
+    end
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def car_params
+    params.require(:car).permit(:user_id, :plate_number, :model, :color, :year, :all_tags)
+  end
 end
--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -4,15 +4,18 @@ class CarsControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @user = User.create!(username: "Example User", email:"user@example.com", password: "foobar", password_confirmation: "foobar")
+    @badboi = User.create!(username: "Bad Boi", email:"bad@boi.com", password: "foobar", password_confirmation: "foobar")
     @car = Car.create!(user_id: @user.id, make: "Ford", model: "Mustang", year: 2000, color: "red", plate_number: "8DEF234")
   end
 
   test "should get new" do
+    log_in_as(@user, password: "foobar")
     get new_car_url
     assert_response :success
   end
 
   test "should create car" do
+    log_in_as(@user, password: "foobar")
     assert_difference('Car.count') do
       post cars_url, params: { car: { user_id: @car.user_id, make: @car.make, model: @car.model, year: @car.year, color: @car.color, plate_number: @car.plate_number } }
     end
@@ -26,20 +29,72 @@ class CarsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    log_in_as(@user, password: "foobar")
     get edit_car_url(@car)
     assert_response :success
   end
 
   test "should update car" do
+    log_in_as(@user, password: "foobar")
     patch car_url(@car), params: { car: { user_id: @car.user_id, make: @car.make, model: @car.model, year: @car.year, color: @car.color, plate_number: @car.plate_number } }
     assert_redirected_to car_url(@car)
   end
 
   test "should destroy car" do
+    log_in_as(@user, password: "foobar")
     assert_difference('Car.count', -1) do
       delete car_url(@car)
     end
 
     assert_redirected_to cars_url
+  end
+
+  test "should redirect if guest tries to create a car" do
+    get new_car_url
+    assert_redirected_to login_url
+    assert_not flash.empty?
+
+    post cars_url, params: { car: { user_id: @car.user_id, make: @car.make, model: @car.model, year: @car.year, color: @car.color, plate_number: @car.plate_number } }
+    assert_redirected_to login_url
+    assert_not flash.empty?
+  end
+
+  test "should redirect if guest tries to edit a car" do
+    get edit_car_url(@car)
+    assert_redirected_to login_url
+    assert_not flash.empty?
+  end
+
+  test "should redirect if guest tries to update car" do
+    patch car_url(@car), params: { car: { user_id: @car.user_id, make: @car.make, model: @car.model, year: @car.year, color: @car.color, plate_number: @car.plate_number } }
+    assert_redirected_to login_url
+    assert_not flash.empty?
+  end
+
+  test "should redirect if guest tries to delete a car" do
+    delete car_url(@car)
+    assert_redirected_to login_url
+    assert_not flash.empty?
+  end
+
+  test "should redirect if non-owner tries to edit car" do
+    log_in_as(@badboi, password: "foobar")
+    get edit_car_url(@car)
+    assert_redirected_to car_url(@car)
+    assert_not flash.empty?
+  end
+
+  test "should redirect if non-owner tries to update car" do
+    log_in_as(@badboi, password: "foobar")
+    patch car_url(@car), params: { car: { user_id: @car.user_id, make: @car.make, model: @car.model, year: @car.year, color: @car.color, plate_number: @car.plate_number } }
+    assert_redirected_to car_url(@car)
+    assert_not flash.empty?
+  end
+
+  test "should redirect if non-owner tries to delete car" do
+    log_in_as(@badboi, password: "foobar")
+    delete car_url(@car)
+    assert_redirected_to car_url(@car)
+    assert_not flash.empty?
   end
 end

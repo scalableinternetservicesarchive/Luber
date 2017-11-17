fa6c0d44ba04d234728876011821bba29a74e782
masoug <masoug@gmail.com>
2017-11-16 18:23:14 -0800

    hotfix to add make as field for car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -99,6 +99,6 @@ class CarsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def car_params
-    params.require(:car).permit(:user_id, :plate_number, :model, :color, :year, :all_tags)
+    params.require(:car).permit(:user_id, :make, :plate_number, :model, :color, :year, :all_tags)
   end
 end

c2d28b95b9c2a5ccf3a2a3063f894eda5b4905ea
carsonkk <carsonkk@gmail.com>
2017-12-04 23:54:52 -0800

    cache nesting for rentals index page

--- app/helpers/rentals_helper.rb
+++ app/helpers/rentals_helper.rb
@@ -2,4 +2,8 @@ module RentalsHelper
   def cache_key_for_rental(rental, owner, car)
     "rental/#{rental.id}/#{rental.updated_at}/#{owner.username}/#{car.make}-#{car.model}"
   end
+
+  def cache_key_for_rentals_index(available_rentals, owners, cars)
+    "rentals_index/#{available_rentals.first}/#{owners.first}/#{cars.first}"
+  end
 end
